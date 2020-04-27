50f923a89a72cbdc2396a5f79119aaf8ebbd3ea0
CirnoV <cirnov59@gmail.com>
2020-04-27 18:21:45 +0900

    feat: add nosleep.js

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import NoSleep from "nosleep.js";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,16 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
+);
+
+const noSleep = new NoSleep();
+document.addEventListener(
+  "click",
+  function enableNoSleep() {
+    document.removeEventListener("click", enableNoSleep, false);
+    noSleep.enable();
+  },
+  false
 );
 
 // https://stackoverflow.com/questions/41869122/touch-events-within-iframe-are-not-working-on-ios
